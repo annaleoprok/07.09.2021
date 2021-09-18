ce38eb6d8369539e81811f4df2c5520600e02147
annaleoprok <Piefish78@gmail.com>
2021-09-18 15:06:58 +0900

    Initial commit

---
+++ src/four_six.java
@@ -0,0 +1,19 @@
+public class four_six {
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new NullPointerException("ошибка");
+        } catch (ArithmeticException e) {
+            System.out.println("1" );
+        }
+        catch (RuntimeException e) {
+            System.out.println("2" );
+        }
+        catch (Exception e) {
+            System.out.println("3" );
+        }
+        System.out.println("4");
+    }
+
+}
+
